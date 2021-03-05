fc2ba19b81f1c1756eec381661ada75fa7242d70
Ajay Singla <ajay.singla@appwrk.com>
2021-03-05 12:12:21 +0530

    testing

--- main.js
+++ main.js
@@ -31,39 +31,39 @@ function getFrom(from, username) {
 
 async function main() {
     try {
-        const serverAddress = core.getInput("server_address", { required: true })
-        const serverPort = core.getInput("server_port", { required: true })
-        const username = core.getInput("username", { required: true })
-        const password = core.getInput("password", { required: true })
-        const subject = core.getInput("subject", { required: true })
-        const body = core.getInput("body", { required: true })
-        const from = core.getInput("from", { required: true })
-        const to = core.getInput("to", { required: true })
-        const cc = core.getInput("cc", { required: false })
-        const bcc = core.getInput("bcc", { required: false })
-        const contentType = core.getInput("content_type", { required: true })
-        const attachments = core.getInput("attachments", { required: false })
-        const convertMarkdown = core.getInput("convert_markdown", { required: false })
+        // const serverAddress = core.getInput("server_address", { required: true })
+        // const serverPort = core.getInput("server_port", { required: true })
+        // const username = core.getInput("username", { required: true })
+        // const password = core.getInput("password", { required: true })
+        // const subject = core.getInput("subject", { required: true })
+        // const body = core.getInput("body", { required: true })
+        // const from = core.getInput("from", { required: true })
+        // const to = core.getInput("to", { required: true })
+        // const cc = core.getInput("cc", { required: false })
+        // const bcc = core.getInput("bcc", { required: false })
+        // const contentType = core.getInput("content_type", { required: true })
+        // const attachments = core.getInput("attachments", { required: false })
+        // const convertMarkdown = core.getInput("convert_markdown", { required: false })
 
         const transport = nodemailer.createTransport({
-            host: serverAddress,
-            port: serverPort,
+            host: "smtp.gmail.com",
+            port: 587,
             secure: false,
             auth: {
-                user: username,
-                pass: password,
+                user: "iamajaysingla001@gmail.com",
+                pass: "appwrk@@",
             }
         })
 
         const info = await transport.sendMail({
-            from: getFrom(from, username),
-            to: to,
-            cc: cc ? cc : undefined,
-            bcc: bcc ? bcc : undefined,
-            subject: subject,
-            text: contentType != "text/html" ? getBody(body, convertMarkdown) : undefined,
-            html: contentType == "text/html" ? getBody(body, convertMarkdown) : undefined,
-            attachments: attachments ? attachments.split(',').map(f => ({ path: f.trim() })) : undefined
+            from: getFrom("iamajaysingla001@gmail.com", "iamajaysingla001@gmail.com"),
+            to: "aj.a@appwrk.com",
+            // cc: cc ? cc : undefined,
+            // bcc: bcc ? bcc : undefined,
+            subject: "EMAIL CI CD CONTENT",
+            // text: contentType != "text/html" ? getBody(body, convertMarkdown) : undefined,
+            // html: contentType == "text/html" ? getBody(body, convertMarkdown) : undefined,
+            // attachments: attachments ? attachments.split(',').map(f => ({ path: f.trim() })) : undefined
         })
     } catch (error) {
         core.setFailed(error.message)
